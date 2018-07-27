66d1901517151c3390674f1508c0e6ed0ab90d41
ekoooo <954408050@qq.com>
2018-07-27 18:13:50 +0800

    update api

--- assets/api.js
+++ assets/api.js
@@ -5,7 +5,7 @@ var Api = {
     // 最新消息
     "newsLatest": "http://news-at.zhihu.com/api/4/news/latest",
     // 热门消息
-    "newsHot": "http://news-at.zhihu.com/api/3/news/hot",
+    "newsHot": "http://news-at.zhihu.com/api/4/news/hot",
     // 过往消息, e.g. http://news-at.zhihu.com/api/4/news/before/20131119
     "newsBefore": "http://news-at.zhihu.com/api/4/news/before/%1",
     // 消息内容, e.g. http://news-at.zhihu.com/api/4/news/#{id}
@@ -14,10 +14,14 @@ var Api = {
     "themes": "http://news-at.zhihu.com/api/4/themes",
     // 主题日报内容, e.g. http://news-at.zhihu.com/api/4/theme/#{id}
     "theme": "http://news-at.zhihu.com/api/4/theme/%1",
+    // 主题日报列表加载更多 1. themeId 2.newsId
+    "themeMore": "http://news-at.zhihu.com/api/4/theme/%1/before/%2",
     // 栏目总览
-    "sections": "http://news-at.zhihu.com/api/3/sections",
+    "sections": "http://news-at.zhihu.com/api/4/sections",
     // 栏目具体消息查看, e.g. http://news-at.zhihu.com/api/3/section/#{id}
-    "section": "http://news-at.zhihu.com/api/3/section/%1",
+    "section": "http://news-at.zhihu.com/api/4/section/%1",
+    //栏目列表加载更多 1. sectionId 2.timestamp
+    "sectionMore": "http://news-at.zhihu.com/api/4/section/%1/before/%2",
     // 新闻额外信息, e.g. http://news-at.zhihu.com/api/4/story-extra/#{id}
     "storyExtra": "http://news-at.zhihu.com/api/7/story-extra/%1",
     // 新闻的推荐者, e.g. http://news-at.zhihu.com/api/4/story/#{id}/recommenders
